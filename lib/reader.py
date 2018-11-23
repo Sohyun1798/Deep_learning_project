bb8b0a5c262fc22fcb691a8cd4da08d30a691b58
chadaeun <chadaeun57@gmail.com>
2018-11-23 18:31:58 +0900

    focus done

--- lib/reader.py
+++ lib/reader.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+
+import numpy as np
 import spacy
 from abc import abstractmethod
 
@@ -12,11 +14,13 @@ class BaseReader(object):
         self.PAD_TOKEN = PAD_TOKEN
         self.nlp = spacy.load('en')
 
-        self.dataset = self.load_dataset(datafile)
+        self.datafile = datafile
+        self.dataset = self.load_dataset()
+        self.folds = None
 
-    def load_dataset(self, datafile):
+    def load_dataset(self):
         fields = self._get_fields()
-        data_dict_generator = self._data_dict_generator(datafile)
+        data_dict_generator = self._data_dict_generator()
 
         examples = [Example.fromdict(data_dict, fields) for data_dict in data_dict_generator]
         fields = {k: v for k, v in fields.values()}
@@ -28,6 +32,9 @@ class BaseReader(object):
             if not field.use_vocab:
                 continue
 
+            if not os.path.isdir(vocab_dir):
+                os.makedirs(vocab_dir)
+
             path = os.path.join(vocab_dir, name)
             if os.path.isfile(path):
                 with open(path, 'rb') as fread:
@@ -38,21 +45,40 @@ class BaseReader(object):
                     pickle.dump(field.vocab, fwrite)
 
     def set_vocabs(self, vocabs):
-        fields = self.dataset.fields
-        for name, field in fields.items():
-            field.vocab = vocabs[name]
+        for name, vocab in vocabs.items():
+            if name in self.dataset.fields:
+                self.dataset.fields[name].vocab = vocab
 
     def get_vocabs(self):
-        return {name: field.vocab for name, field in self.dataset.fields.items()}
+        vocabs = {}
+
+        for name, field in self.dataset.fields.items():
+            if field.use_vocab:
+                vocabs[name] = field.vocab
+
+        return vocabs
 
     def get_dataset_iterator(self, batch_size, **kwargs):
-        return iter(Iterator(self.dataset, batch_size, **kwargs))
+        return Iterator(self.dataset, batch_size, **kwargs)
+
+    def get_cross_val_dataset_iterator(self, batch_size, k_fold=5, **kwargs):
+        if self.folds is None:
+            total_len = len(self.dataset)
+            fold_idx = [total_len // k_fold * i_fold for i_fold in range(k_fold)]
+            fold_idx.append(total_len)
+
+            folds = []
+            for i in range(k_fold):
+                folds.append(Dataset(self.dataset.examples[fold_idx[i]:fold_idx[i+1]], self.dataset.fields))
+            self.folds = folds
+
+        return [Iterator(fold, batch_size, **kwargs) for fold in self.folds]
 
     def get_vocab(self, key):
         return self.dataset.fields[key].vocab
 
     @abstractmethod
-    def _data_dict_generator(self, datafile):
+    def _data_dict_generator(self):
         raise NotImplementedError('You should use child class')
 
     @abstractmethod
@@ -85,7 +111,7 @@ class BaseReader(object):
 
 
 class UIUCReader(BaseReader):
-    def __init__(self, datafile, PAD_TOKEN='<pad>', batch_size=32, pad_size=60):
+    def __init__(self, datafile, PAD_TOKEN='<pad>', pad_size=60):
         super(UIUCReader, self).__init__(datafile, PAD_TOKEN=PAD_TOKEN, pad_size=pad_size)
         self.nlp = spacy.load('en')
 
@@ -93,8 +119,8 @@ class UIUCReader(BaseReader):
         doc = self.nlp(sentence)
         return [token.lower_ for token in doc]
 
-    def _data_dict_generator(self, datafile):
-        with open(datafile, 'r') as fread:
+    def _data_dict_generator(self):
+        with open(self.datafile, 'r') as fread:
             for line in fread:
                 category, sentence = line.strip().split(maxsplit=1)
                 category = category.split(':')[0]
@@ -108,3 +134,40 @@ class UIUCReader(BaseReader):
         }
 
         return fields
+
+
+class QFocusReader(BaseReader):
+    def __init__(self, datafile, PAD_TOKEN='<pad>', pad_size=60):
+        super(QFocusReader, self).__init__(datafile, PAD_TOKEN=PAD_TOKEN, pad_size=pad_size)
+        self.nlp = spacy.load('en')
+
+    def preprocess(self, sentence):
+        doc = self.nlp(sentence)
+        return [token.lower_ for token in doc]
+
+    def _data_dict_generator(self):
+        with open(self.datafile, 'r') as fread:
+            for line in fread:
+                raw_words = self.preprocess(line.strip())
+
+                if raw_words[0] == 'IMPL'.lower() or raw_words.count('#') != 1:
+                    continue
+
+                focus_index = -1
+                words = []
+                for i, raw_word in enumerate(raw_words):
+                    if raw_word == '#':
+                        focus_index = i
+                    else:
+                        words.append(raw_word)
+
+                yield {'focus': focus_index, 'words': words}
+
+    def _get_fields(self):
+        fields = {
+            'words': ('words', Field(pad_token=self.PAD_TOKEN, batch_first=True, sequential=True, use_vocab=True, fix_length=self.pad_size)),
+            'focus': ('focus', Field(batch_first=True, sequential=False, use_vocab=False))
+        }
+
+        return fields
+
